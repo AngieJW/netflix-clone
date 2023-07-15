8b7bc0e78359b02c56c1cd8cf7358b74a89ea682
Angie Duhard <angie.duhard@gmail.com>
2023-07-15 23:10:55 +0200

    added banner

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -2,8 +2,30 @@ import React from 'react'
 import './Banner.css'
 
 function Banner() {
+
+    function truncate(string, n) {
+      return string?.length > n ? string.substr(0, n - 1) + '...' : string;
+    }
+
   return (
-    <div>Banner</div>
+    <header className='banner'
+      style={{
+        backgroundSize: "cover",
+        backgroundImage: `url('https://as2.ftcdn.net/v2/jpg/03/71/56/17/1000_F_371561715_LVI4qVJ2hyWMDXdqJNGdktggEzjQuC15.jpg')`,
+        backgroundPosition: "center center"
+      }}
+    >
+      <div className="banner__contents">
+        <h1 className="banner__title">Movie Name</h1>
+        <div className="banner__buttons">
+          <button className='banner__button'>Play</button>
+          <button className='banner__button'>My List</button>
+        </div>
+        <h2 className="banner__description">{truncate(`This is a test description This is a test description This is a test description This is a test description This is a test description This is a test description`, 150)}</h2>
+      </div>
+      <div className="banner--fadeBottom"/>
+      <img src="" alt="" />
+    </header>
   )
 }
 
