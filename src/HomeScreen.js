bd521e6e4491c5bcc736fd5c81c78210d3a938c4
Angie Duhard <angie.duhard@gmail.com>
2023-07-15 22:21:14 +0200

    created navbar and useeffect for background

---
+++ src/HomeScreen.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import "./HomeScreen.css";
+import Nav from "./Components/Nav"
+import Banner from "./Components/Banner";
+
+function HomeScreen() {
+  return (
+    <div className='homeScreen'>
+      <Nav />
+
+      <Banner />
+
+      {/* {ROWS} */}
+
+    </div>
+  )
+}
+
+export default HomeScreen
