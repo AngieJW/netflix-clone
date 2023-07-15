9fb43cd178f558c22fd1ca5b7e5957a1b83235f7
Angie Duhard <angie.duhard@gmail.com>
2023-07-15 21:21:10 +0200

    created app for netflix clone

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyD_cRxatOSZaci63Yrt5iul66eroaJfyec",
+  authDomain: "netflix-clone-dbe46.firebaseapp.com",
+  projectId: "netflix-clone-dbe46",
+  storageBucket: "netflix-clone-dbe46.appspot.com",
+  messagingSenderId: "917981479280",
+  appId: "1:917981479280:web:49e64db7a512dc737ecfb5",
+  measurementId: "G-DGPEEEDW7W"
+};
